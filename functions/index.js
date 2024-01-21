82821f21aa05a326937fd9304c97194a55a751e4
Petar Petrov <ppetrov@vexelon.net>
2024-01-21 22:30:27 +0100

    Add twitter dependency and bump x crap code

--- functions/index.js
+++ functions/index.js
@@ -5,8 +5,8 @@ const trasher = require('./trasher');
 const config = require('./config.json');
 
 // --- runs every 3 minutes (TODO: ever 1 hour)
-exports.xpublish = onSchedule("*/3 * * * *", 
-    async (event) => x.publishxcrap(config, event));
+// exports.xpublish = onSchedule("*/3 * * * *", 
+//     async (event) => x.publishxcrap(config, event));
 
 // --- runs every 1 hour
 exports.redditdownloadandsave = onSchedule("0 * * * *", 
--- functions/x.js
+++ functions/x.js
@@ -1,31 +1,55 @@
 const { logger } = require("firebase-functions");
 const admin = require('firebase-admin');
+const { TwitterApi } = require('twitter-api-v2')
+
+async function getPost(config) {
+    const { firebase, reddit } = config;
+
+    const db = admin.firestore();
+    const redditPostsRef = db.collection(firebase.collectionName);
+
+    // Query unreposted Reddit posts ordered by timestamp (oldest first)
+    const postsQuery = await redditPostsRef.where('is_on_x', '==', false).orderBy('created_at').limit(1).get();
+
+    if (!postsQuery.empty) {
+        const unposted = postsQuery.docs[0];
+
+        // TODO:
+        // await unposted.ref.update({ is_on_x: true });
+
+        logger.debug('Found something to post to X:', unposted.data());
+
+        return unposted.data();
+    } else {
+        logger.info('Nothing to post');
+    }
+
+    return null;
+}
+
+async function doX(post, config) {
+    const { firebase, twitter } = config;
+
+    logger.debug('Crapping on X...');
+
+    const client = new TwitterApi(twitter);
+
+    await client.v1.tweet(`${post.title} ${post.url}`);
+}
 
 exports.publishxcrap = async (config, event) => {
     logger.debug('**** publishing on x');
 
     try {
-        const { firebase, reddit } = config;
-
-        const db = admin.firestore();
-        const redditPostsRef = db.collection(firebase.collectionName);
-
-        // Query unreposted Reddit posts ordered by timestamp (oldest first)
-        const postsQuery = await redditPostsRef.where('is_on_x', '==', false).orderBy('created_at').limit(1).get();
-
-        if (!postsQuery.empty) {
-            const unposted = postsQuery.docs[0];
-
-            // TODO:
-            // await unposted.ref.update({ is_on_x: true });
-
-            logger.debug('*** publishing X post:', unposted.data());
-        } else {
-            logger.info('Nothing to post');
-        }
+        await doX(
+            await getPost(config), 
+            config
+        );
     } catch (error) {
         logger.error('Error publishing on x:', error);
     }
 
     return null;
 };
+
+// --- TEST ---
